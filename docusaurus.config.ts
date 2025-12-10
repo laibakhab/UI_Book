a39ea40fbb1ef3d9e729a1b235e3d9d735025172
laibakhab <asifalikhan4485@gmail.com>
2025-12-10 23:07:18 +0500

    sec

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,15 +14,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ui-book.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'laibakhab', // Usually your GitHub org/user name.
+  projectName: 'UI_Book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
